184dc5a2c7eb1e11f252b7b5aa53dd13ff31c8d7
zsimo <mohamedzoraa100@gmail.com>
2023-09-14 17:09:20 +0100

    respos

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
     };
   }, [])
   return (
-    <>
+    <div>
       <div className={`fixed duration-500 transition-transform left-0 w-screen h-screen bg-[#032324] z-[2000] text-6xl flex flex-col items-center justify-center ${pageLoaded ? "-translate-y-full" : "translate-y-0"}`}>
         {/* <div className="w-1/2 h-2/3">
           <img className="h-full w-full object-contain" src={logo} alt="logo" />
@@ -66,7 +66,7 @@ function App() {
       <Tech />
       <Work />
 
-    </>
+    </div>
   )
 }
 
