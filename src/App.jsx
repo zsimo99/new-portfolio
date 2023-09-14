85c1ad697cf1ea3eb31de30ec1f4bfe5a3021e3c
zsimo <mohamedzoraa100@gmail.com>
2023-09-14 16:43:59 +0100

    respos

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   const [section, setSection] = useState("home")
   const [show, setShow] = useState(false)
   useEffect(() => {
-
-    window.onload = () => {
+    const handleLoad = () => {
       // The entire page, including all images and assets, has finished loading
       setPageLoaded(true);
     };
+    window.addEventListener("load", handleLoad)
 
     const divs = document.querySelectorAll("section")
     let prevPos = 0
@@ -40,12 +40,13 @@ function App() {
     window.addEventListener("scroll", handleScroll)
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      window.onload = null;
+      window.removeEventListener("load", handleLoad)
+
     };
   }, [])
   return (
     <>
-      <div className={`fixed duration-500 transition-[top] top-0 left-0 w-screen h-screen bg-[#032324] z-[2000] text-6xl flex flex-col items-center justify-center ${pageLoaded && "-top-full"}`}>
+      <div className={`fixed duration-500 transition-transform top-0 left-0 w-screen h-screen bg-[#032324] z-[2000] text-6xl flex flex-col items-center justify-center ${pageLoaded && "-translate-y-full"}`}>
         <div className="w-1/2 h-2/3">
           <img className="h-full w-full object-contain" src={logo} alt="logo" />
         </div>
