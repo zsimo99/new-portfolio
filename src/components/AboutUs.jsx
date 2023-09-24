34c2507c8cde434d8916e62b33f91635c45a0ab7
zsimo <mohamedzoraa100@gmail.com>
2023-09-24 11:28:20 +0100

    fixes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -13,28 +13,28 @@ const AboutUs = () => {
             id="aboutUs"
             className=" py-40  bg-[#032324]"
         >
-            <div className="container mx-auto px-4 flex max-lg:flex-col gap-10 items-center">
+            <div className="container mx-auto px-4 flex max-lg:flex-col-reverse gap-10 items-center">
                 <div className=" lg:basis-1/2">
-                    <h2 className="lg:hidden mb-8 text-5xl md:text-6xl xl:text-7xl  font-extrabold text-[#40dcdf] mb-4 hide duration-700 transition-[opacity,transform] ">
-                        About Us:
-                    </h2>
-                    <div className="border-8 border-[#309092]">
+                    <div className="border-8 border-[#309092] max-w-md xl:max-w-lg">
                         <Video autoPlay loop>
                             <source src={video} type="video/mp4" />
                         </Video>
                     </div>
                 </div>
                 <div className="lg:basis-1/2">
-                    <h2 className="max-lg:hidden text-5xl md:text-6xl xl:text-7xl  font-extrabold text-[#40dcdf] mb-4 hide duration-700 transition-[opacity,transform] ">
+                    <h2 className=" text-5xl md:text-6xl xl:text-7xl  font-extrabold text-[#40dcdf] mb-4 hide duration-700 transition-[opacity,transform] ">
                         About Us:
                     </h2>
-                    <p className="text-base text-start md:text-lg xl:text-xl md:text-justify font-medium text-white pl-10 hide duration-700 transition-[opacity,transform]">
+                    <p className="text-base text-start md:text-lg xl:text-xl md:text-justify font-medium text-white pl-4 lg:pl-10 hide duration-700 transition-[opacity,transform]">
                         At ZMT Agency, we take pride in crafting digital experiences that leave a lasting impact. Our team is dedicated to delivering excellence in web design and web application development. With a keen eye for aesthetics and a passion for innovation, we transform concepts into captivating websites and powerful web applications. Our commitment to quality and creativity has earned us a reputation for turning ideas into reality. Explore our portfolio to witness the expertise and dedication that defines us at ZMT Agency.
                     </p>
-                    <a target="_blank" rel="noreferrer" href="https://www.fiverr.com/zakariami?source=gig_page" className=" duration-700 transition-[opacity,transform] px-10 py-2 md:px-14 md:py-4 xl:px-16 xl:py-5 bg-slate-500 mt-4 block w-fit ms-auto rounded-md text-2xl font-bold text-white bg-gradient-to-r to-[#309092] from-[#6CC6C8] relative z-0">
-                        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md  opacity-0 hover:opacity-100 transition-opacity duration-200 w-full h-full bg-[#ffffff30] z-10" />
-                        Hire Us</a>
                 </div>
+
+            </div>
+            <div className="container mx-auto p-4">
+                <a target="_blank" rel="noreferrer" href="https://www.fiverr.com/zakariami?source=gig_page" className=" mt-8 duration-700 transition-[opacity,transform] px-10 py-2 md:px-14 md:py-4 xl:px-16 xl:py-5 bg-slate-500 block w-fit ms-auto rounded-md text-2xl font-bold text-white bg-gradient-to-r to-[#309092] from-[#6CC6C8] relative z-0">
+                    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md  opacity-0 hover:opacity-100 transition-opacity duration-200 w-full h-full bg-[#ffffff30] z-10" />
+                    Hire Us</a>
             </div>
         </section>
     );
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,7 +2,7 @@
 // eslint-disable-next-line react/prop-types
 const Service = ({ title, icon, desc, id }) => {
     return (
-        <div className={`max-w-xs bg-[#103c3d] px-5 py-14 text-white flex items-center justify-between space-y-3 flex-col  hide duration-700 transition-[opacity,transform] ${id === 1 && "xl:-translate-y-10"}`}>
+        <div className={`max-w-xs bg-[#103c3d] px-5 py-14 text-white flex items-center justify-between space-y-3 flex-col  hide duration-700 transition-[opacity,transform] min-h-[350px] ${id === 1 && "xl:-translate-y-10"}`}>
             <div className="w-24 p-4 bg-[#0c3131] rounded-[40px] rotate-45">
                 <img className="w-20 -rotate-45" src={icon} alt="" />
             </div>
